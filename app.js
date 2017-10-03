11b5c4e2f3e43fd64185c520a6dc9d2f622f6cbd
RamKar <30436956+RamKar@users.noreply.github.com>
2017-10-03 12:00:52 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+var app = angular.module("test", ["ngAria", "ngAnimate", "ngMaterial"]);
+
+app
+    .controller("testController", function($scope, $mdDialog) {
+      return ($scope.showDialog = function(evt) {
+        return $mdDialog.show({
+          controller: function($scope) {
+            return angular.extend($scope, {
+              user: {
+                Name: "",
+                email: "ipsum@lorem.com",
+                visibility: "",
+                Email: "",
+                Owner:"",
+                role:"",
+                state:""
+              },
+              closeDialog: function() {
+                return $mdDialog.hide();
+              }
+            });
+          },
+          templateUrl: "dialogContent.tmpl.html",
+          targetEvent: evt
+        });
+      });
+    });
